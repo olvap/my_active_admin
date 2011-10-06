c06ca4aef99e2413d9c70ac5ec8ef5d18da3463f
Olvap <oldani.pablo@gmail.com>
2011-10-05 21:29:28 -0300

    agrego plugin de personas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111005201512) do
+ActiveRecord::Schema.define(:version => 20111005235210) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -97,6 +97,56 @@ ActiveRecord::Schema.define(:version => 20111005201512) do
     t.decimal  "api",         :precision => 7, :scale => 2
     t.decimal  "otro",        :precision => 7, :scale => 2
     t.date     "fecha"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_addresses", :force => true do |t|
+    t.string   "street"
+    t.integer  "number"
+    t.string   "other"
+    t.string   "email"
+    t.string   "telefono"
+    t.integer  "taddress_id"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_civils", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_people", :force => true do |t|
+    t.string   "name"
+    t.date     "born"
+    t.string   "doc"
+    t.date     "dead"
+    t.text     "observations"
+    t.integer  "civil_id"
+    t.integer  "tdoc_id"
+    t.integer  "city_id"
+    t.integer  "sexo_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_sexos", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_taddresses", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "personas_tdocs", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
